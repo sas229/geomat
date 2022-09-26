a26bb2b117fdb2bd1eb9dfebce8c5b8f1615b98d
sas229 <sas229@cam.ac.uk>
2022-09-26 09:12:29 +0100

    Added initial files.

---
+++ main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <Eigen/Eigen>
+#include "umat.cpp"
+
+int main()
+{      
+    int ndi = 3;
+    int nshr = 3;
+    int ntens = ndi*nshr;
+    double stress[ntens] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int nstatv = 2;
+    double statev[nstatv] = {7, 8};
+    double ddsdde[ndi*nshr] = {3, 2, 1, 6, 5, 4, 9, 8, 7};
+    double sse, spd, scd, rpl;
+    double ddsddt[ntens], drplde[ntens];
+    double drpldt;
+    double stran[ntens] = {0};
+    double dstran[ntens] = {0.1, 0.1, 0.1, 0.0, 0.0, 0.0};
+    double time[2];
+    double dtime, temp, dtemp;
+    double predef, dpred;
+    char cmname[] = "MCC";
+    int nprops = 2;
+    double props[nprops];
+    double coords[3] = {0.0, 0.0, 0.0};
+    double drot;
+    double pnewdt, celent;
+    double dfgrd0;
+    double dfgrd1;
+    int noel = 1;
+    int npt = 1;
+    int layer;
+    int kspt;
+    int kstep;
+    int kinc;
+
+    for (int i=0; i < 10; ++i) {
+        umat(
+            stress,
+            statev,
+            ddsdde,
+            &sse,
+            &spd,
+            &scd,
+            &rpl,
+            ddsddt,
+            drplde,
+            &drpldt,
+            stran,
+            dstran,
+            time,
+            &dtime,
+            &temp,
+            &dtemp,
+            &predef,
+            &dpred,
+            cmname,
+            &ndi,
+            &nshr,
+            &ntens,
+            &nstatv,
+            props,
+            &nprops,
+            coords,
+            &drot,
+            &pnewdt,
+            &celent,
+            &dfgrd0,
+            &dfgrd1,
+            &noel,
+            &npt,
+            &layer,
+            &kspt,
+            &kstep,
+            &kinc
+        );
+    }
+
+    // Test Eigen library.
+    int n = 10;
+    Eigen::VectorXi v(n);
+    for(int i = 0; i < n;  i++)
+    {
+        v[i] = i;
+    }
+    std::cout << "vector: " << v.transpose() << "\n";
+    
+}
---
+++ umat.cpp
@@ -0,0 +1,180 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <Eigen/Eigen>
+#include <vector>
+#include <memory>
+#include <typeinfo>
+
+class Model {
+    public:
+        Model(std::string name) {
+            _name = name;
+        }
+        std::string get_name(void) {return _name;}
+        virtual double get_lambda(void) {return _lambda;}
+    private:
+        std::string _name;
+        double _lambda;
+};
+
+class MCC : public Model {
+    public: 
+       MCC(std::string name = "MCC") : Model(name) {}
+       double get_lambda(void) {return 2.541;} 
+    private:
+        int _nparams = 6;
+};
+
+class SMCC : public Model {
+    public: 
+       SMCC(std::string name = "SMCC") : Model(name) {}
+       double get_lambda(void) {return 0.442;} 
+    private:
+        int _nparams = 9;
+};
+
+// Global booleans to control model instantiation.
+bool first_call = true;
+bool model_instantiated = false;
+
+// Global unique pointer to store a pointer to the chosen model.
+std::unique_ptr<Model> model;
+
+// Debug output to file.
+std::ofstream debug("umat.log");
+
+void instantiate_model(char* cmname) {
+    // Set first_call boolean to false so that model instantiation is not repeated on subsequent calls to the umat function.
+    first_call = false;
+
+    // Compare model name supplied with available model names. Instantiate model if found, otherwise report error.
+    if (strcmp(cmname, "MCC") == 0) {
+        model.reset(new MCC);   
+    } else if (strcmp(cmname, "SMCC") == 0) {
+        model.reset(new SMCC);    
+    } else {
+        debug << "Error: Model name given not implemented. Check name given in input file.\n";
+        return;
+    }
+    model_instantiated = true;
+    debug << cmname << " model instantiated.\n";
+}
+
+/** Abaqus umat interface.
+ * 
+ * This function allows Abaqus to call the library of user-defined materials defined within this library. 
+ * Fortran passes variables by reference by default, hence pointers are passed for all variables. In the 
+ * stress and strain arrays and in the matrices `ddsdde', `ddsddt', and `drplde', direct components are 
+ * stored first, followed by shear components. There are `ndi' direct and `nshr' engineering shear components.
+ * At a minimum the function must define `ddsdde', `stress', `statev'. 
+ * 
+ * @brief Interface to allow Abaqus to call umat material models via a C++ implementation.
+ * @param stress Stress tensor at the beginning of the increment to be updated.
+ * @param statev An array of solution dependent state variables to be updated.
+ * @param ddsdde Jacobian matrix to be updated.
+ * @param sse Specific elastic strain energy.
+ * @param spd Specific plastic dissipation.
+ * @param scd Specific creep dissipation.
+ * @param rpl Volumetric heat generation per unit time.
+ * @param ddsddt Variation of the stress increments with respect to the temperature.
+ * @param drplde Variation of rpl with respect to the strain increments.
+ * @param drpldt Variation of rpl with respect to the temperature.
+ * @param stran The total strains at the beginning of the increment.
+ * @param dstran Array of strain increments.
+ * @param time Value of step time (1) and total time (2) at the beginning of the increment.
+ * @param dtime Time increment.
+ * @param temp Temperature at the start of the increment.
+ * @param predef Array of predefined field variables at this point at the start of the increment, interpolated from nodes.
+ * @param dpred Array of increments of predefined field variables.
+ * @param cmname User-defined material name (avoid ABQ_ prefix).
+ * @param ndi Number of direct stress components at this point.
+ * @param nshr Number of engineering shear stress components at this point.
+ * @param ntens Size of the stress or strain component array (`ndi' + `nshr').
+ * @param nstatv Number of solution-dependent state variables that are associated with this material type.
+ * @param props User-specified array of material constants associated with this user material.
+ * @param nprops Number of user-defined material constants associated with this user material.
+ * @param coords An array containing the coordinates of this point.
+ * @param drot Rotation increment matrix.
+ * @param celent Characteristic element length, which is a typical length of a line across an element for a first-order element; it is half of the same typical length for a second-order element.
+ * @param dfgrd0 Array containing the deformation gradient at the beginning of the increment.
+ * @param dfgrd1 Array containing the deformation gradient at the end of the increment.
+ * @param noel Element number.
+ * @param npt Integration point number.
+ * @param layer Layer number.
+ * @param kspt Section point number within the current layer.
+ * @param kstep Procedure type key; nlgeom boolean; linear perturbation boolean.
+ * @param kinc Increment number.
+ * */
+extern "C" void umat(
+    double *stress,
+    double *statev,
+    double *ddsdde,
+    double *sse,
+    double *spd,
+    double *scd,
+    double *rpl,
+    double *ddsddt,
+    double *drplde,
+    double *drpldt,
+    double *stran,
+    double *dstran,
+    double *time,
+    double *dtime,
+    double *temp,
+    double *dtemp,
+    double *predef,
+    double *dpred,
+    char *cmname,
+    int *ndi,
+    int *nshr,
+    int *ntens,
+    int *nstatv,
+    double *props,
+    int *nprops,
+    double *coords,
+    double *drot,
+    double *pnewdt,
+    double *celent,
+    double *dfgrd0,
+    double *dfgrd1,
+    int *noel,
+    int *npt,
+    int *layer,
+    int *kspt,
+    int *kstep,
+    int *kinc
+    )
+{   
+    if (first_call) {
+        // Instantiate model if first call to umat function.
+        instantiate_model(cmname);
+
+        // Set parameters.
+
+        // Set state variables.
+
+        
+    } 
+    if (model_instantiated) {
+        // Perform stress integration.
+        std::string name;
+        name = model->get_name();
+        debug << "Model name: " << name << "\n";
+
+        double lambda;
+        lambda = model->get_lambda();
+        debug << "Lambda = " << lambda << "\n";
+
+        // Do stress integration with existing model.
+        stress[0] = 99;
+        debug << "ddsdde: " << ddsdde[0];
+        for (int i = 1; i < *ntens; ++i) {
+            debug  << ", " << ddsdde[i];
+        }
+        debug << "\n";   
+    }
+    
+}
+
