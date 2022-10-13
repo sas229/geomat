dadae2f3ee506c3061b7132458a42a8423897c80
Sam Stanier <sas229@cam.ac.uk>
2022-10-13 19:50:37 +0100

    Modified Elastic class log messages.

--- src/utilities/Elastic/src/Elastic.cpp
+++ src/utilities/Elastic/src/Elastic.cpp
@@ -9,7 +9,7 @@ void Elastic::compute_isotropic_linear_elastic_matrix(void) {
     D_e(0,0) = D_e(1,1) = D_e(2,2) += C*(1.0-nu); 
     D_e(0,1) = D_e(0,2) = D_e(1,2) = D_e(1,0) = D_e(2,0) = D_e(2,1) += C*nu;
     D_e(3,3) = D_e(4,4) = D_e(5,5) += C*(1.0-2.0*nu)/2.0; 
-    PLOG_DEBUG << "Isotropic linear elastic matrix computed.";
+    PLOG_INFO << "Isotropic linear elastic matrix computed.";
 }
 
 void Elastic::compute_anisotropic_linear_elastic_matrix(void) {
@@ -23,7 +23,7 @@ void Elastic::compute_anisotropic_linear_elastic_matrix(void) {
     D_e(2,0) = D_e(0,2) = D_e(2,1) = D_e(1,2) = -nu_v/E_v;
     D_e(3,3) = D_e(4,4) += 1/G_v;
     D_e(5,5) = 2.0*(1+nu_h)/E_h;
-    PLOG_DEBUG << "Anisotropic linear elastic matrix computed.";
+    PLOG_INFO << "Anisotropic linear elastic matrix computed.";
 }
 
 void Elastic::compute_simplified_anisotropic_linear_elastic_matrix(void) {
@@ -38,7 +38,7 @@ void Elastic::compute_simplified_anisotropic_linear_elastic_matrix(void) {
     D_e(2,0) = D_e(0,2) = D_e(2,1) = D_e(1,2) = C*-nu_h/alpha;
     D_e(3,3) = D_e(4,4) += C*2.0*(1.0+nu_h)/alpha;
     D_e(5,5) = C*2.0*(1.0+nu_h)/std::pow(alpha,2);
-    PLOG_DEBUG << "Simplified anisotropic linear elastic matrix computed.";
+    PLOG_INFO << "Simplified anisotropic linear elastic matrix computed.";
 }
 
 Eigen::Matrix<double, 6, 6> Elastic::get_elastic_matrix(void) {
