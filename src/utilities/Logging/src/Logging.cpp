c16cf67831e785b77de4400af6988ebac1b04abe
sas229 <sas229@cam.ac.uk>
2023-07-18 17:35:51 +0100

    Fixed duplicative logger in Logging module.

--- src/utilities/Logging/src/Logging.cpp
+++ src/utilities/Logging/src/Logging.cpp
@@ -1,24 +1,31 @@
 #include "Logging.hpp"
 
-void Logging::initialise_log(std::string level) {
-    static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender;
-    if (level == "verbose") {
-        plog::init(plog::verbose, &consoleAppender);
-    } else if (level == "debug") {
-        plog::init(plog::debug, &consoleAppender);
-    } else if (level == "info") {
-        plog::init(plog::info, &consoleAppender);
-    } else if (level == "warning") {
-        plog::init(plog::warning, &consoleAppender);
-    } else if (level == "error") {
-        plog::init(plog::error, &consoleAppender);
-    } else if (level == "fatal") {
-        plog::init(plog::fatal, &consoleAppender);
-    } else if (level == "none") {
-        plog::init(plog::none, &consoleAppender);
+void Logging::initialise_log(std::string severity) {
+    // If no logger found, initialise the logger.
+    auto log = plog::get();
+    if (log == NULL) {
+        static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender;
+        if (severity == "verbose") {
+            plog::init(plog::verbose, &consoleAppender);
+        } else if (severity == "debug") {
+            plog::init(plog::debug, &consoleAppender);
+        } else if (severity == "info") {
+            plog::init(plog::info, &consoleAppender);
+        } else if (severity == "warning") {
+            plog::init(plog::warning, &consoleAppender);
+        } else if (severity == "error") {
+            plog::init(plog::error, &consoleAppender);
+        } else if (severity == "fatal") {
+            plog::init(plog::fatal, &consoleAppender);
+        } else if (severity == "none") {
+            plog::init(plog::none, &consoleAppender);
+        } else {
+            plog::init(plog::warning, &consoleAppender);
+            PLOG_WARNING << "Unrecognised log severity set. Possible options include \"verbose\", \"debug\", \"info\", \"warning\", \"error\", \"fatal\", or \"none\". Defaulting to \"error\".";
+            plog::get()->setMaxSeverity(plog::error);
+        }
     } else {
-        plog::init(plog::warning, &consoleAppender);
-        PLOG_WARNING << "Unrecognised log level set. Possible options include \"verbose\", \"debug\", \"info\", \"warning\", \"error\", \"fatal\", or \"none\". Defaulting to \"error\".";
-        plog::get()->setMaxSeverity(plog::error);
+        // Otherwise adjust the severity.
+        log->setMaxSeverity(plog::severityFromString(severity.c_str()));
     }
 }
--- test.py
+++ test.py
@@ -120,7 +120,7 @@ ea_increment = ea_max/increments
 Delta_epsilon_tilde = np.array([ea_increment, 0.0, 0.0, 0.0, 0.0, 0.0])
 axial_strain = np.arange(0, increments*ea_increment, ea_increment)
 
-model = C2MC(log_severity="verbose", parameters=parameters, state=state)
+model = C2MC(log_severity="fatal", parameters=parameters, state=state)
 print("Model name: {}".format(model.name))
 print("Model type: {}".format(model.type))
 model.set_sigma_prime_tilde(stress)
